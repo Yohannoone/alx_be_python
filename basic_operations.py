e88b16d3f6cebfb753765b6324d1e268767609b0
Yohan <moriatyohan86@gmail.com>
2025-07-11 04:44:23 -0700

    adding some little changes to basic_operations.py file

--- basic_operations.py
+++ basic_operations.py
@@ -5,7 +5,7 @@ sum_result = number1 + number2
 diff_result = number1 - number2
 prod_result = number1 * number2
 
-print(f"Sum of {number1} and {number2} is {sum_result}")
-print(f"Difference of {number1} and {number2} is {diff_result}")
-print(f"Product of {number1} and {number2} is {prod_result}")
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {diff_result}")
+print(f"Multiplication of {number1} and {number2} is {prod_result}")
 
