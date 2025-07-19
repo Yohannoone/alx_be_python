df5e2e09bb40b70b09bb8243178e485c4098a502
Yohan <moriatyohan86@gmail.com>
2025-07-19 12:22:29 -0700

    adding all the files in ont time, and it is obvious what is their purpose just be seeing the name

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+# Prompt User for Pattern Size
+size = int(input("Enter the size of the pattern: "))
+
+# Draw the Pattern
+row = 0
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+    print()
+    row += 1
